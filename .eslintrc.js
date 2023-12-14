98fc4dacc446d675735e54fd8acb066db4ae1551
Mai Mostafa <mayamostafa@gmail.com>
2023-12-14 14:40:16 -0500

    solution created + cleanup

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+require('@rushstack/eslint-config/patch/modern-module-resolution');
+module.exports = {
+  extends: ['@microsoft/eslint-config-spfx/lib/profiles/react'],
+  parserOptions: { tsconfigRootDir: __dirname },
+  rules : {
+    "@microsoft/spfx/no-async-await": "off",
+    "@typescript-eslint/no-floating-promises" : "off",
+    "@typescript-eslint/no-unnecessary-type-constraint": "off",
+    "@typescript-eslint/explicit-function-return-type" : "off",
+    "@typescript-eslint/no-unnecessary-type-assertion" : "off",
+    "@typescript-eslint/typedef" : "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-inferrable-types": "off",
+    "@typescript-eslint/no-explicit-any": "off"
+  }
+};
---
+++ src/webparts/lunchroomSupervisors/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface ILunchroomSupervisorsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'LunchroomSupervisorsWebPartStrings' {
+  const strings: ILunchroomSupervisorsWebPartStrings;
+  export = strings;
+}
