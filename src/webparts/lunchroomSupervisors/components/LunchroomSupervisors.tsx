d4c4aa8c3dd8d7f23fafccf1b37332f1be82c79b
Mai Mostafa <mayamostafa@gmail.com>
2023-12-14 14:43:13 -0500

    solution created + class to fnc cleanup

---
+++ src/webparts/lunchroomSupervisors/components/LunchroomSupervisors.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import styles from './LunchroomSupervisors.module.scss';
+import { ILunchroomSupervisorsProps } from './ILunchroomSupervisorsProps';
+
+export default function LunchroomSupervisors(props: ILunchroomSupervisorsProps){
+  return (
+    <div className={styles.lunchroomSupervisors}>Test</div>
+  );
+}
+
+
